29c3219b99cd2fde055b152a46a9162a2234eb48
It's Trésor <bernardalade92@gmail>
2026-04-02 09:59:54 +0000

    ouo

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import { AnimatePresence } from 'framer-motion';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
+import NotAvailable from './pages/NotAvailable';
 import Loader from './components/Loader';
 
 function AnimatedRoutes() {
@@ -17,10 +14,7 @@ function AnimatedRoutes() {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="services" element={<Services />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotAvailable />} />
         </Route>
       </Routes>
     </AnimatePresence>
